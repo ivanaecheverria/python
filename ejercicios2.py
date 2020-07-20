7675c409f059da6f9bd28f0c1b25ef294f7b2901
Ivana Echeverría <ivana.amarantos@gmail.com>
2020-07-20 02:59:54 -0300

    Update ejercicios2.py

--- ejercicios2.py
+++ ejercicios2.py
@@ -46,17 +46,43 @@
 #Si la contraseña coincide con un valor específico, el programa imprime “Bienvenido Pepe”. 
 #Si la contraseña es incorrecta, imprime “Acceso denegado”.
 
-usuario = input("Ingrese su usuario: ")
+#usuario = input("Ingrese su usuario: ")
 
-if usuario != "Pepe":
-    print("Acceso denegado")
+#if usuario != "Pepe":
+#    print("Acceso denegado")
 
-elif usuario == "Pepe":
-    password = input("Ingrese su contraseña: ")
-    if password != "1234":
-        print("Acceso denegado")
-    elif password == "1234":
-        print("Bienvenido Pepe")
+#elif usuario == "Pepe":
+#    password = input("Ingrese su contraseña: ")
+#    if password != "1234":
+#        print("Acceso denegado")
+#    elif password == "1234":
+#        print("Bienvenido Pepe")
 
+#Hacer un programa que reciba un numero, y haga lo siguiente
+#Si el numero es mayor a 100, imprimir “Mayor a 100”
+#Pero si es 666, imprimir “Numero del diablo”
+#Si es menor a 10, imprimir “Tiene un solo digito”
+#Pero si esta entre 10 y 100, y es par, imprimir: “Es numero par”. Si es impar, imprimir “Es numero impar”
+#Finalmente, si el numero termina en 5 (e.g. 15, 25, 105) imprimir “Nivel Dios”
+
+numero = int(input("Ingresar un numero: "))
+if numero == 666:  
+    print("Numero del diablo")
+
+if numero >= 10 and numero <=100:
+    if numero%2==0:
+        print("Es numero par")
+    elif numero%2==1:
+        print ("Es numero impar")
+
+if numero > 100: 
+    print("mayor a 100")
+
+elif numero < 10:
+    print("Tiene un solo digito")
+
+numero = numero-5
+if numero%10 == 0:
+    print("Nivel Dios")
 
 
